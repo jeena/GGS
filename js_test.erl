06de3c2f7e693c8dcdfb111be244ae0cdc868a69
Jeena Paradies <spam@jeenaparadies.net>
2011-01-25 15:06:44 +0100

    changed file structure and build

--- js_test.erl
+++
@@ -1,7 +0,0 @@
--module(js_test).
--export([run/0]).
-run() ->
-    erlang_js:start(),
-    {ok, Port} = js_driver:new(),
-    ok = js:define(Port, <<"function helloworld(name){return 'Hello, ' + name}">>),
-    js:call(Port, <<"helloworld">>, [<<"Generic Game Server">>]).

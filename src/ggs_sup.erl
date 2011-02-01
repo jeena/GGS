72e411aee9a991a0ea8aa65f78312e52b33d1ad7
Jonatan Pålsson <jonte@Zepto.(none)>
2011-02-01 15:04:15 +0100

    Added _working_ supervisor behaviour

--- src/ggs_sup.erl
+++ src/ggs_sup.erl
@@ -2,19 +2,28 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/1]).
 
 %% Supervisor callbacks
 -export([init/1]).
 -define(SERVER, ?MODULE).
 
-start_link() ->
-    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+start_link(Port) ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, [Port]).
 
-init([]) ->
-    Server = {ggs_server, {ggs_server, start_link, []},
-        permanent, 2000, worker, [ggs_server]},
+init([Port]) ->
+    Server =    {ggs_server, 
+                    {ggs_server, start_link, [Port]},
+                    permanent, 
+                    2000, 
+                    worker, 
+                    [ggs_server]
+                },
     Children = [Server],
-    RestartStrategy = {one_for_one, 0, 1},
+
+    RestartStrategy = { one_for_one, % Restart only crashing child
+                        10,          % Allow ten crashes per..
+                        1            % 1 second, then crash supervisor.
+                      },
     {ok, {RestartStrategy, Children}}.
 

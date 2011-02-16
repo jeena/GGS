e09b3cbd143facecb87714029fb31c6972c4b453
Kallfaktorn <mattiaspgames@gmail.com>
2011-02-16 17:28:55 +0100

    Refined ggs_player.

--- src/ggs_player.erl
+++ src/ggs_player.erl
@@ -1,22 +1,26 @@
+-module(ggs_player).
+-export([start_link/1, notify/3, get_token/1, stop/1]).
 
+%% @doc This module handles communication between a player and GGS. This module is
+%%responsible for: 
+%% * the storage of the player socket, player token and a table token.
 
-
-%% @spec start_link(GameToken::string()) -> socket()
-%% @doc Spawn a new player process.
-start_link(Socket) -> end.
+%% @doc Spawns a new player process.
+%% @spec start_link(Socket::socket()) -> ok
+start_link(Socket) -> not_implemented.
 
 
 %% @spec notify(Player::Pid(), From::Pid(), 
 %%              {Command::String(), Message::string()}) -> ok
 %% @doc send a message to a player.
-notify(Player, From, Message) -> end.
+notify(Player, From, Message) -> not_implemented.
 
 
 %% @spec get_token() -> string()
 %% @doc Get the player token.
-get_token() -> end.
+get_token() -> not_implemented.
 
 
 %% @spec stop(Table::pid()) -> Reason::string()
 %% @doc Properly terminates the process.
-stop(Table) -> end.
+stop(Table) -> not_implemented.

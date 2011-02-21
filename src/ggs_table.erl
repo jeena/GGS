073c4d49b01b057094ce47e5b8ffaf4a12e9c923
Jeena Paradies <spam@jeenaparadies.net>
2011-02-22 00:35:54 +0100

    merged rewrite and master

--- src/ggs_table.erl
+++ src/ggs_table.erl
@@ -17,7 +17,6 @@
 	 stop/1,
 	 notify/3]).
 
--include_lib("eunit/include/eunit.hrl").
 
 %% ----------------------------------------------------------------------
 % API implementation
@@ -120,80 +119,3 @@ terminate(_Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-%% @TODO: Please put these tests in a separate file. We can't compile this file if
-%% they contain errors from switching vms
-%% ----------------------------------------------------------------------
-% Tests
-
-%<<<<<<< HEAD
-%start_link_test() ->
-%	Table = start_link(),
-%	?assertNot(Table =:= undefined).
-	
-%add_player_test() ->
-%	Table = start_link(),
-%	Player = test_player,
-%	add_player(Table, Player),
-%	{ok, [Player]} = gen_server:call(Table, get_player_list).
-	
-%remove_player_test() ->
-%	Table = start_link(),
-%	Player = test_player,
-%	Player2 = test_player2,
-%	add_player(Table, Player),
-%	{ok, [Player]} = gen_server:call(Table, get_player_list),
-%	add_player(Table, Player2),
-%	{ok, [Player2, Player]} = gen_server:call(Table, get_player_list),	
-%	remove_player(Table, Player),
-%	{ok, [Player2]} = gen_server:call(Table, get_player_list),
-%	remove_player(Table, Player2),
-%	{ok, []} = gen_server:call(Table, get_player_list).	
-%	
-%stop_test() ->
-%	Table = start_link(),
-%	ok = stop(Table).
-
-% @private
-%notify_test() ->
-%	Table = start_link(),
-%	Player = test_player,
-%	Message = {server, define, "function helloWorld(x) {  }"},
-%	ok = notify(Table, Player, Message).
-%=======
-%%start_link_test() ->
-%	Table = start_link("123", none),
-%	?assertNot(Table =:= undefined).
-%	
-%add_player_test() ->
-%	Table = start_link("123", none),
-%	Player = test_player,
-%	add_player(Table, Player),
-%	{ok, [Player]} = gen_server:call(Table, get_player_list).
-	
-%remove_player_test() ->
-%	Table = start_link("123", none),
-%	Player = test_player,
-%	Player2 = test_player2,
-%	add_player(Table, Player),
-%	{ok, [Player]} = gen_server:call(Table, get_player_list),
-%	add_player(Table, Player2),
-%	{ok, [Player2, Player]} = gen_server:call(Table, get_player_list),	
-%	remove_player(Table, Player),
-%	{ok, [Player2]} = gen_server:call(Table, get_player_list),
-%	remove_player(Table, Player2),
-%	{ok, []} = gen_server:call(Table, get_player_list).	
-%	
-%stop_test() ->
-%	Table = start_link("123", none),
-%	ok = stop(Table).
-%
-%% @private
-%notify_test() ->
-%	Table = start_link("123", none),
-%	Player = test_player,
-%	Message = {server, define, "function helloWorld(x) {  }"},
-%	ok = notify(Table, Player, Message).
-%>>>>>>> jonte_rewrite
-	%Message2 = {game, "helloWorld", "test"},
-	%ok = notify(Table, Player, Message2).
-	
--- src/js_helper.erl
+++
@@ -1,8 +0,0 @@
--module(js_helper).
--export([test/0, echo/1]).
-
-test() ->
-	{ok, test}.
-	
-echo(String) ->
-	{ok, String}.

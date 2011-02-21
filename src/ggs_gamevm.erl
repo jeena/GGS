08e0be70e2c15446ebac33aba64d66b95093cfe2
Jeena Paradies <spam@jeenaparadies.net>
2011-02-21 20:54:43 +0100

    fixed test with js callback

--- src/ggs_gamevm.erl
+++ src/ggs_gamevm.erl
@@ -119,10 +119,15 @@ user_command_test() ->
     define(GameVM, "var t = '';\nfunction userCommand(user, command, args) { t = user + command + args; }\n"),
     user_command(GameVM, "'jeena", "thecommand", "theargs'"),
     ?assertMatch(<<"'jeenathecommandtheargs'">>, gen_server:call(GameVM, {eval, "t;"})).
+
+to_string({A, B, C}) ->
+  "{" ++ integer_to_list(A) ++ ", " ++ integer_to_list(B) ++ ", " ++ integer_to_list(C) ++ "}".
     
 user_command2_test() ->
     GameVM = start_link(test_table),
     define(GameVM, "var t = '';\nfunction userCommand(user, command, args) { t = callErlang('erlang time') + ''; }\n"),
-    user_command(GameVM, "jeena", "test", ""),
-    io:format("test ~p~n", [binary_to_list(gen_server:call(GameVM, {eval, "t;"}))]),
-    ?assertMatch(<<"rt">>, gen_server:call(GameVM, {eval, "t;"})).
+    user_command(GameVM, "", "", ""),
+	T = to_string(erlang:time()),
+    io:format("test ~p~n", [T]),
+    ?assertMatch(T, binary_to_list(gen_server:call(GameVM, {eval, "t;"}))).
+

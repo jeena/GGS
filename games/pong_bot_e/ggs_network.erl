f8b383232b70fed22ef95a4397aa1fec9e5319d8
Kallfaktorn <mattiaspgames@gmail.com>
2011-04-26 16:13:56 +0200

    players are actually doing something

--- games/pong_bot_e/ggs_network.erl
+++ games/pong_bot_e/ggs_network.erl
@@ -3,6 +3,7 @@
 -export([read/2, send_command/3]).
 
 connect() ->
+    %{ok,Socket} = gen_tcp:connect("ggs.jeena.net", 9000,[{active, false}]),
     {ok,Socket} = gen_tcp:connect("localhost", 9000,[{active, false}]),
     Socket.
 
